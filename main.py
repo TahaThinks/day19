4574493049a833e4f761571cd40edb177f96f6ec
TahaThinks <tahaa4587@gmail.com>
2024-02-12 11:03:39 +0400

    Free Movement using "w,s,a,d"

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ screen = Screen()
 
 def move_forward():
     tim.forward(10)
+def move_backward():
+    tim.backward(10)
+def rotate_clockwise():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+def rotate_anticlockwise():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
 
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key='a', fun=rotate_clockwise)
+screen.onkey(key='d', fun=rotate_anticlockwise)
 screen.exitonclick()
