580f48e4f8208824bce8fdc8540a5973b6bec2f0
TahaThinks <tahaa4587@gmail.com>
2024-02-12 11:15:28 +0400

    Refine Code

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ tim = Turtle()
 tim.pensize(5)
 screen = Screen()
 
+
 def move_forward():
     tim.forward(10)
+
+
 def move_backward():
     tim.backward(10)
+
+
 def rotate_clockwise():
     new_heading = tim.heading() + 10
     tim.setheading(new_heading)
+
+
 def rotate_anticlockwise():
     new_heading = tim.heading() - 10
     tim.setheading(new_heading)
+
+
 def clear_screen():
     tim.clear()
     tim.penup()
