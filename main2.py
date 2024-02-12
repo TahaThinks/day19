a231ac8881fa8410020eb94d3cb858a75fb79244
TahaThinks <tahaa4587@gmail.com>
2024-02-12 11:36:44 +0400

    Create 5 Turtles

--- main2.py
+++ main2.py
@@ -1,7 +1,21 @@
 from turtle import Turtle, Screen
-
-tim = Turtle()
+import random
 screen =  Screen()
 screen.setup(width=500, height=400)
+
+# user_bet = screen.textinput(title = "Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+# print(user_bet)
+
+colors = ["red","orange","yellow","green","blue","purple"]
+y_axis = [-70,-40,-10,20,50,80]
+x_axis = [5,10,15]
+
+for turtle_index in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.goto(x=-240, y=y_axis[turtle_index])
+    tim.color(colors[turtle_index])
+
+
 
 screen.exitonclick()
