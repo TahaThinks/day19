8b02dad47b1677d673f3b9fdc0ab6449d9d58204
TahaThinks <tahaa4587@gmail.com>
2024-02-12 21:15:08 +0400

    For loop

--- main2.py
+++ main2.py
@@ -1,21 +1,29 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 screen =  Screen()
 screen.setup(width=500, height=400)
 
-# user_bet = screen.textinput(title = "Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-# print(user_bet)
+user_bet = screen.textinput(title = "Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+print(user_bet)
+is_race_on = False
 
 colors = ["red","orange","yellow","green","blue","purple"]
 y_axis = [-70,-40,-10,20,50,80]
-x_axis = [5,10,15]
+all_turtles = []
 
 for turtle_index in range(0,6):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.goto(x=-240, y=y_axis[turtle_index])
-    tim.color(colors[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-240, y=y_axis[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    random_distance = random.randint(0,10)
+    turtle.forward(random_distance)
 
 screen.exitonclick()
